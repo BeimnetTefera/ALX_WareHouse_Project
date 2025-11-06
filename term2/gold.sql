094c39e3b36ace16c1c82b12af5038f49357f829
Beimnet Tefera <asefabeimnet1@gmail.com>
2025-11-06 10:16:19 +0300

    Update course names in SQL comments

--- term2/gold.sql
+++ term2/gold.sql
@@ -4,14 +4,14 @@ WITH CoursePerformance AS (
         TRIM(last_name) AS last_name,
         TRIM(course_name) AS course_name,
 
-        -- ✅ Failed assignments (submitted but not passed)
+        --Failed assignments (submitted but not passed)
         STRING_AGG(
             CASE 
                 WHEN is_assignment_passed = 'No' THEN TRIM(assignment_name)
             END, ', '
         ) AS failed_assignments,
 
-        -- ✅ Missed assignments (not submitted at all)
+        -- Missed assignments (not submitted at all)
         STRING_AGG(
             CASE 
                 WHEN is_assignment_submitted = 'No' THEN TRIM(assignment_name)
@@ -45,35 +45,35 @@ SELECT
     TRIM(cp.email) AS [Email],
     TRIM(cp.age_range) AS [Age_Range],
 
-    -- BUS 200
+    -- BUSINESS FINANCE
     MAX(CASE WHEN cp.course_name = 'BUS 200: BUSINESS FINANCE' THEN cp.no_of_submissions END) AS [BUS 200: # Submitted],
     MAX(CASE WHEN cp.course_name = 'BUS 200: BUSINESS FINANCE' THEN cp.no_of_assignments_passed END) AS [BUS 200: # Passed],
     COALESCE(MAX(CASE WHEN cp.course_name = 'BUS 200: BUSINESS FINANCE' THEN cp.failed_assignments END), 'All Good ') AS [BUS 200: Failed Assignments],
     COALESCE(MAX(CASE WHEN cp.course_name = 'BUS 200: BUSINESS FINANCE' THEN cp.missed_assignments END), 'All Good ') AS [BUS 200: Missed Assignments],
     MAX(CASE WHEN cp.course_name = 'BUS 200: BUSINESS FINANCE' THEN cp.lms_overall_score END) AS [BUS 200: LMS Score],
 
-    -- ENT 110
+    -- INTRODUCTION TO VENTURE CREATION
     MAX(CASE WHEN cp.course_name = 'ENT 110: INTRODUCTION TO VENTURE CREATION' THEN cp.no_of_submissions END) AS [ENT 110: # Submitted],
     MAX(CASE WHEN cp.course_name = 'ENT 110: INTRODUCTION TO VENTURE CREATION' THEN cp.no_of_assignments_passed END) AS [ENT 110: # Passed],
     COALESCE(MAX(CASE WHEN cp.course_name = 'ENT 110: INTRODUCTION TO VENTURE CREATION' THEN cp.failed_assignments END), 'All Good ') AS [ENT 110: Failed Assignments],
     COALESCE(MAX(CASE WHEN cp.course_name = 'ENT 110: INTRODUCTION TO VENTURE CREATION' THEN cp.missed_assignments END), 'All Good ') AS [ENT 110: Missed Assignments],
     MAX(CASE WHEN cp.course_name = 'ENT 110: INTRODUCTION TO VENTURE CREATION' THEN cp.lms_overall_score END) AS [ENT 110: LMS Score],
 
-    -- QNT 102
+    -- INTRODUCTION TO STATISTICS
     MAX(CASE WHEN cp.course_name = 'QNT 102: INTRODUCTION TO STATISTICS' THEN cp.no_of_submissions END) AS [QNT 102: # Submitted],
     MAX(CASE WHEN cp.course_name = 'QNT 102: INTRODUCTION TO STATISTICS' THEN cp.no_of_assignments_passed END) AS [QNT 102: # Passed],
     COALESCE(MAX(CASE WHEN cp.course_name = 'QNT 102: INTRODUCTION TO STATISTICS' THEN cp.failed_assignments END), 'All Good ') AS [QNT 102: Failed Assignments],
     COALESCE(MAX(CASE WHEN cp.course_name = 'QNT 102: INTRODUCTION TO STATISTICS' THEN cp.missed_assignments END), 'All Good ') AS [QNT 102: Missed Assignments],
     MAX(CASE WHEN cp.course_name = 'QNT 102: INTRODUCTION TO STATISTICS' THEN cp.lms_overall_score END) AS [QNT 102: LMS Score],
 
-    -- SE 101
+    -- INTRODUCTION TO COMPUTING
     MAX(CASE WHEN cp.course_name = 'SE 101: INTRODUCTION TO COMPUTING' THEN cp.no_of_submissions END) AS [SE 101: # Submitted],
     MAX(CASE WHEN cp.course_name = 'SE 101: INTRODUCTION TO COMPUTING' THEN cp.no_of_assignments_passed END) AS [SE 101: # Passed],
     COALESCE(MAX(CASE WHEN cp.course_name = 'SE 101: INTRODUCTION TO COMPUTING' THEN cp.failed_assignments END), 'All Good ') AS [SE 101: Failed Assignments],
     COALESCE(MAX(CASE WHEN cp.course_name = 'SE 101: INTRODUCTION TO COMPUTING' THEN cp.missed_assignments END), 'All Good ') AS [SE 101: Missed Assignments],
     MAX(CASE WHEN cp.course_name = 'SE 101: INTRODUCTION TO COMPUTING' THEN cp.lms_overall_score END) AS [SE 101: LMS Score],
 
-    -- SE 102
+    -- FOUNDATIONS OF LINUX AND VERSION CONTROL
     MAX(CASE WHEN cp.course_name = 'SE 102: FOUNDATIONS OF LINUX AND VERSION CONTROL' THEN cp.no_of_submissions END) AS [SE 102: # Submitted],
     MAX(CASE WHEN cp.course_name = 'SE 102: FOUNDATIONS OF LINUX AND VERSION CONTROL' THEN cp.no_of_assignments_passed END) AS [SE 102: # Passed],
     COALESCE(MAX(CASE WHEN cp.course_name = 'SE 102: FOUNDATIONS OF LINUX AND VERSION CONTROL' THEN cp.failed_assignments END), 'All Good ') AS [SE 102: Failed Assignments],
